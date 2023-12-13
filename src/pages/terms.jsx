2fbf2ec02d67b3d76df611c278a61bd8426a130b
freshconceptstudio <73487121+freshconceptstudio@users.noreply.github.com>
2023-12-13 12:54:39 -0700

    Update terms.jsx

--- src/pages/terms.jsx
+++ src/pages/terms.jsx
@@ -3,45 +3,54 @@ import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 
 const PrivacyPolicyContent = `
-  <h1>[Your App Name] Privacy Policy</h1>
-  <p><strong>Last updated:</strong> [Date]</p>
+  <h1 className="text-3xl font-bold mb-6">Convey Privacy Policy</h1>
 
-  <h2>1. Introduction</h2>
-  <p>Welcome to [Your App Name]! This Privacy Policy is meant to help you understand how we collect, use, and safeguard your information.</p>
+  <p className="mb-4"><strong>Last updated:</strong> 12/13/2023</p>
 
-  <h2>2. Information We Collect</h2>
-  <p>We may collect personal information that you provide directly when using our app, such as [list specific data like names, email addresses, etc.].</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">1. Introduction</h2>
+  <p className="mb-4">Welcome to Convey! This Privacy Policy is meant to help you understand how we collect, use, and safeguard your information.</p>
 
-  <h2>3. Automatically Collected Information</h2>
-  <p>We may collect certain information automatically, including but not limited to [list specific data like device information, IP addresses, usage statistics, etc.].</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">2. Information We Collect</h2>
+  <p className="mb-4">We gather personal information directly from you while using our app. This includes, but is not limited to, essential details like names and phone numbers. Additionally, for enhanced user experiences, we may collect specific health information and event updates provided by you.</p>
 
-  <h2>4. How We Use Your Information</h2>
-  <p>We use the collected information for [describe the purposes, e.g., providing and improving the app, personalizing user experience, etc.].</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">3. Automatically Collected Information</h2>
+  <p className="mb-4">As part of our commitment to providing a seamless and personalized experience, we automatically gather various types of information when you interact with our app. This encompasses essential data such as device information, enabling us to optimize our services for your specific device and enhance compatibility.</p>
 
-  <h2>5. Sharing of Your Information</h2>
-  <p>We may share your information with [list any third parties or categories of third parties, if applicable, e.g., service providers, analytics providers].</p>
+  <p className="mb-4">Furthermore, we collect IP addresses to analyze regional usage patterns and improve the overall performance and security of our app. This information is instrumental in ensuring a reliable and efficient service for all users.</p>
 
-  <h2>6. Security</h2>
-  <p>We take reasonable measures to protect your information from unauthorized access or disclosure.</p>
+  <p className="mb-4">In addition to the above, we leverage usage statistics to gain insights into how our app is utilized. This helps us make informed decisions for enhancements and updates, ultimately tailoring our app to better meet the needs and preferences of our user community.</p>
 
-  <h2>7. Your Choices</h2>
-  <p>You can [describe any user choices, opt-outs, or preferences available].</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">4. How We Use Your Information</h2>
+  <p className="mb-4">At Convey, we utilize the information we collect to create a personalized and tailored experience for you within the app. This includes, but is not limited to, customizing content, features, and recommendations based on your preferences, usage patterns, and provided details.</p>
+  <p className="mb-4">Moreover, your information is instrumental in facilitating seamless communication. We enable you to share timely updates and event-related information with others through the app. This feature enhances the collaborative and interactive nature of our platform, allowing users to stay connected and informed in real-time.</p>
+  <p className="mb-4">By harnessing the power of the data you entrust to us, we aim to not only enhance your individual experience but also foster a sense of community and engagement among our users. Rest assured, we prioritize the security and privacy of your information in every aspect of our service.</p>
 
-  <h2>8. Children’s Privacy</h2>
-  <p>Our app is not intended for individuals under the age of [specify the age] and we do not knowingly collect personal information from children.</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">5. Sharing of Your Information</h2>
+  <p className="mb-4">At Convey, transparency and trust are paramount, and we want to ensure you have a clear understanding of how we handle your information. In certain instances, we may share your information with trusted service providers who play a crucial role in delivering and enhancing our services.</p>
 
-  <h2>9. Changes to This Privacy Policy</h2>
-  <p>We may update our Privacy Policy from time to time. You will be notified of any changes by [describe how users will be notified].</p>
+  <p className="mb-4">These service providers are carefully selected partners that assist us in various aspects such as hosting, analytics, customer support, and other essential functionalities. By collaborating with these entities, we can maintain the reliability, efficiency, and security of the Convey app, ensuring a seamless and robust user experience.</p>
 
-  <h2>10. Contact Us</h2>
-  <p>If you have any questions or concerns about this Privacy Policy, please contact us at [provide contact information].</p>
+  <p className="mb-4">Rest assured that our partnerships with service providers are established under strict agreements that prioritize the protection and confidentiality of your information. We hold our partners to the same high standards of security and privacy that you expect from Convey.</p>
 
-  <p>By using our app, you agree to the terms outlined in this Privacy Policy.</p>
+  <p className="mb-4">If you have any specific questions about the service providers we collaborate with or would like more details on how your information is shared, please refer to our detailed Privacy Policy or feel free to reach out to our support team for clarification.</p>
 
-  <p>[Your Company/Organization Name]<br>
-  [Your Address]<br>
-  [Your Email Address]<br>
-  [Your Phone Number]</p>
+  <h2 className="text-xl font-bold mt-6 mb-2">6. Security</h2>
+  <p className="mb-4">At Convey, the security of your information is of utmost importance to us. We employ a comprehensive range of robust security measures, each meticulously designed to safeguard your data from unauthorized access, disclosure, or any form of compromise.</p>
+
+  <h2 className="text-xl font-bold mt-6 mb-2">7. Your Choices</h2>
+  <p className="mb-4">At Convey, we believe in empowering our users with choices and control over their experience. You have the ability to tailor your interactions and preferences within the app to suit your needs. If you ever want to stop updates, simply text "STOP".</p>
+
+  <h2 className="text-xl font-bold mt-6 mb-2">8. Children’s Privacy</h2>
+  <p className="mb-4">Our app is not intended for individuals under the age of 17 and we do not knowingly collect personal information from children.</p>
+
+  <h2 className="text-xl font-bold mt-6 mb-2">9. Changes to This Privacy Policy</h2>
+  <p className="mb-4">We may update our Privacy Policy from time to time. The latest policy will be found at this url.</p>
+
+  <h2 className="text-xl font-bold mt-6 mb-2">10. Contact Us</h2>
+  <p className="mb-4">If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:support@conveystatus.com" className="text-blue-500">support@conveystatus.com</a>.</p>
+
+  <p className="mt-6">By using our app, you agree to the terms outlined in this Privacy Policy.</p>
+
 `;
 
 const Terms = () => {
@@ -52,10 +61,10 @@ const Terms = () => {
       </Head>
       <Header />
       <main>
-        <section>
+        <Container className="relative">
           <h1>Terms of Service</h1>
           <div dangerouslySetInnerHTML={{ __html: PrivacyPolicyContent }} />
-        </section>
+        </Container>
       </main>
       <Footer />
     </>

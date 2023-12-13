54ef68b0b0a694353db3ea50f681968cd8f982cb
freshconceptstudio <73487121+freshconceptstudio@users.noreply.github.com>
2023-12-13 14:01:28 -0700

    Privacy

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -1,8 +1,10 @@
+// pages/terms.js
 import Head from 'next/head';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { PrivacyPolicyContent } from '@/components/PrivacyPolicyContent';
-import { Container } from '@/components/Container'
+import PrivacyPolicyContent from '@/components/PrivacyPolicyContent';  // Correct import
+
+import { Container } from '@/components/Container';
 
 const Terms = () => {
   return (
@@ -13,7 +15,6 @@ const Terms = () => {
       <Header />
       <main>
         <Container className="relative">
-          <h1>Terms of Service</h1>
           <PrivacyPolicyContent />
         </Container>
       </main>
